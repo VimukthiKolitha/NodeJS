ce8ac15e2cdd21f272adf36bfd0259dac5b929f1
VimukthiKolitha <vimukthikolitah@gmail.com>
2024-12-20 13:23:28 +0530

    server control

--- buildNodeServer/serverControl.js
+++ buildNodeServer/serverControl.js
@@ -1,8 +1,12 @@
+import { fileRead } from "./readFile.js";
 
 export const server = (req,res) =>{
     if(req.url === '/' && req.method === 'GET')
     {
-        res.writeHead(200,{'Content-Type':'text/html'});
-        res.end(`<h1>Hi vimukthi</h1>`);
+        fileRead('./index.html',res,'text/html');
+    }
+    if(req.url === '/index.css' && req.method === 'GET')
+    {
+       fileRead('./index.css',res,'text/css');
     }
 };
