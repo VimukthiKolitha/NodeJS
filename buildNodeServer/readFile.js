8d6da519df0cc9c524f9ed6357d8c33bc2b6de32
VimukthiKolitha <vimukthikolitah@gmail.com>
2024-12-20 13:25:31 +0530

    small upgrade in read filr

--- buildNodeServer/readFile.js
+++ buildNodeServer/readFile.js
@@ -1,18 +1,23 @@
 import { fileURLToPath} from "node:url";
 import { dirname,join} from "node:path";
-import { readFile } from "node:fs";
+import { readFile } from "node:fs/promises";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 
-export const fileRead = async(path) =>{
+export const fileRead = async(path,res,contentType) =>{
 
     try {
         
-        const data = await readFile(join(path))
-            return data;
+        const data = await readFile(join(__dirname,path))
+        res.writeHead(200,{'Content-Type':`${contentType}`});
+        res.end(data);
     } catch (error) {
-        console.log(`<h1>error: ${error}</h1>`)
+
+        res.writeHead(200,{'Content-Type':`${contentType}`});
+        res.end(`<h1>error</h1>`);
+
+        console.log(error);
     }
 };
