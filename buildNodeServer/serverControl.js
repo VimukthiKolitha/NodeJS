038c348d2b97b5a60d1f58c7d1de338226d1a0b0
VimukthiKolitha <vimukthikolitah@gmail.com>
2024-12-21 21:55:08 +0530

    updated

--- buildNodeServer/serverControl.js
+++ buildNodeServer/serverControl.js
@@ -1,12 +1,25 @@
 import { fileRead } from "./readFile.js";
+import {exten} from "./extwntionType.js"
+import {log} from "console";
 
 export const server = (req,res) =>{
+
+    if(req.url === '/favicon.ico')
+    {
+        return ;
+    }
     if(req.url === '/' && req.method === 'GET')
     {
         fileRead('./index.html',res,'text/html');
     }
-    if(req.url === '/index.css' && req.method === 'GET')
-    {
-       fileRead('./index.css',res,'text/css');
-    }
+   else if(req.method == 'GET'){
+     const extention = String (req.url).split(".");
+     fileRead(
+        `${req.url}`,
+        res,
+        exten(extention[extention.length - 1])
+     );
+
+     log('amila =>' , exten(extention[extention.length - 1]))
+   }
 };
