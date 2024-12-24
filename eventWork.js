ceb14dd6c45b13f0d0abf0c00d06bf7ee080c549
VimukthiKolitha <vimukthikolitah@gmail.com>
2024-12-24 20:19:34 +0530

    event loop

--- eventWork.js
+++ eventWork.js
@@ -1,14 +1,39 @@
+import {readFile} from "node:fs";
+
 console.log('vimukthi 1');
 console.log('vimukthi 2');
 
+
 setTimeout(()=>{
-    console.log('vimukthi 3');
+    console.log('timer 1');
 },3)
 console.log('vimukthi 4');
+
+//high priority
+const a = new Promise((data,err) =>{
+    data("promise");
+})
+a.then((data) =>{
+    console.log(data);
+})
+
 console.log('vimukthi 5');
+
+
+//low priority
+readFile('./hey.txt',(data,err)=>{
+    if(err)
+    {
+     console.log(err);
+    }else{
+        console.log(data.toString());
+    }
+});
+
 console.log('vimukthi 6');
+
 setTimeout(()=>{
-    console.log('vimukthi 7');
+    console.log('timer 2');
 },3)
 
 console.log('vimukthi 8');
