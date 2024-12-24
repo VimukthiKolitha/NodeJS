dfbe8c4702f703fcf636c0a31ef874acb3ca71df
VimukthiKolitha <vimukthikolitah@gmail.com>
2024-12-24 12:43:59 +0530

    how code executed work

---
+++ eventWork.js
@@ -0,0 +1,14 @@
+console.log('vimukthi 1');
+console.log('vimukthi 2');
+
+setTimeout(()=>{
+    console.log('vimukthi 3');
+},3)
+console.log('vimukthi 4');
+console.log('vimukthi 5');
+console.log('vimukthi 6');
+setTimeout(()=>{
+    console.log('vimukthi 7');
+},3)
+
+console.log('vimukthi 8');
