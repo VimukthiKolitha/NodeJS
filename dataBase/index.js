978d05fdbc7aa1c590de3959443b01cb5b1efc07
VimukthiKolitha <vimukthikolitah@gmail.com>
2024-12-24 10:38:31 +0530

    DB connection create

--- dataBase/index.js
+++ dataBase/index.js
@@ -1,1 +1,40 @@
-//crating data base
+//crating data base connection 
+
+import {createConnection} from "mysql2";
+import {log} from 'node:console';
+
+//using xampp
+
+//create connection
+const db = createConnection({
+    host:'localhost',
+    port:'3306',
+    user:'root',
+    password:'',
+    database:'student'
+});
+
+//check DB connect or not
+db.connect((err) =>{
+    if(err)
+    {
+       log('error DB not connect'); 
+    }else{
+
+        log('connected');
+
+        //write query
+        const SQLquery ="SELECT * FROM `students`";
+
+        //run query
+        db.query(SQLquery,(err,data)=>{
+            if(err)
+            {
+                log('error:',err);
+            }else{
+                log(data);
+            }
+        });
+
+    }
+});
