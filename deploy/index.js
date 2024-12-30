eae94b55c22823077bb99e96eeb61b3a0c0c5877
VimukthiKolitha <vimukthikolitah@gmail.com>
2024-12-30 12:56:14 +0530

    main

--- deploy/index.js
+++ deploy/index.js
@@ -1,32 +1,54 @@
 
-import {createServer} from "node:http";
+//to deploy this side go to render.com 
+import { createServer } from "node:http";
 import { log } from "node:console";
-import {dirname,join} from "node:path";
-import {pool} from "workerpool";
-//install worker pool(npm i workerpool)
+import { join } from "node:path";
+import workerpool from "workerpool";
+import { fileURLToPath } from "node:url";
 
+// Resolve __dirname
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = join(__filename, "..");
 
-//worker pool instent
-pool(join,(__dirname,'./file-read-worker.js'));
-createServer((req,res) =>{
+// Initialize the worker pool
+const fileReadPool = workerpool.pool(join(__dirname, "./file-read-worker.js"));
 
-   if(req.url === '/')
-   {
-    res.writeHead(200,{'content-type':'text/html'});
-    fileReadPool.exec('html',['index.html']);
-    then((result) =>{
-        res.end(result);
-    }).catch((err)=>{
-        log(err);
-    }).then(()=>{
-        pool.terminate()  //terminate all workers when done
-    })
-    
-   }
+createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
 
-     
-}).listen(4000,() =>{
-    log('running....!');
-})
+    fileReadPool
+      .exec("html", ["index.html"]) // Call the worker's `html` method
+      .then((result) => {
+        res.end(result); // Respond with the file content
+      })
+      .catch((err) => {
+        log(`Error: ${err.message}`);
+        res.statusCode = 500;
+        res.end("Internal Server Error");
+      });
+  }else if(req.url === '/about'){
+
+    res.writeHead(200, { "content-type": "text/html" });
+
+    fileReadPool
+      .exec("html", ["about.html"]) // Call the worker's `html` method
+      .then((result) => {
+        res.end(result); // Respond with the file content
+      })
+      .catch((err) => {
+        log(`Error: ${err.message}`);
+        res.statusCode = 500;
+        res.end("Internal Server Error");
+      });
+
+  }
+  
+  else {
+    res.writeHead(404);
+    res.end("Not Found");
+  }
+  
+}).listen(4000, () => {
+  log("Server is running on http://localhost:4000");
+});
